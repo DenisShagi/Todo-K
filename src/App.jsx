05eef0b90bf3b3364a006a340a71fd7b60d22c70
DenisShag <denis.shagim@icloud.com>
2025-02-25 22:08:52 +0500

    style: Transferred the layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import NewTaskForm from './components/NewTaskForm'
+import TaskList from './components/TaskList'
+import Footer from './components/Footer'
+
 function App() {
-	return <></>
+	return (
+		<section className='todoapp'>
+			<NewTaskForm />
+			<section className='main'>
+        <TaskList />
+				<Footer />
+			</section>
+		</section>
+	)
 }
 
 export default App
